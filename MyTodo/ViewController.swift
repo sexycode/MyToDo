4a76e8f3ff1321462224a352cda10e152f381155
sexycode <sexycode@gmail.com>
2019-05-30 17:21:36 +0900

    Initial Commit

---
+++ MyTodo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodo
+//
+//  Created by sexycode on 30/05/2019.
+//  Copyright © 2019 sexycode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
